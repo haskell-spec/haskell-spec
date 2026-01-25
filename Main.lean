369e916d9a00809e2a9db8309963a6d5a589cef5
David Binder <david.binder@uni-tuebingen.de>
2026-01-25 17:22:03 +0100

    Add simple lexer functionality (#124)

--- Main.lean
+++ Main.lean
@@ -1,4 +1,17 @@
 import HaskellSpec
 
-def main : IO Unit :=
-  IO.println s!"not implemented"
+import HaskellSpec.Lexer.Haskell.Combined
+
+def process_file(filepath : System.FilePath) : IO Unit := do
+  IO.println s!"Processing file {filepath}"
+  let file_content ← IO.FS.readFile filepath
+  let tokens := lex_haskell file_content
+  let printed_tokens := reprStr tokens
+  IO.println s!"{printed_tokens}"
+
+
+
+def main(args : List String): IO Unit :=
+  match args with
+  | [fp] => process_file fp
+  | _ => IO.println s!"Call the executable with exactly one argument"
